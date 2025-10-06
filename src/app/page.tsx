f87ae326f75032f5a24f795deb4ecbfab527390d
Luqman Herifa <luqmanherifa@gmail.com>
2025-10-06 12:33:03 +0700

    Add Categories and CategoryCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/sections/Hero";
+import Categories from "@/components/sections/Categories";
 import Templates from "@/components/sections/Templates";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Categories />
       <Templates />
     </>
   );
---
+++ src/components/sections/Categories.tsx
@@ -0,0 +1,64 @@
+import CategoryCard from "@/components/ui/CategoryCard";
+
+const categoriesData = [
+  {
+    title: "Saghara Labs",
+    image: "/saghara-card.png",
+    link: "#",
+  },
+  {
+    title: "Saghara Labs",
+    image: "/saghara-card.png",
+    link: "#",
+  },
+  {
+    title: "Saghara Labs",
+    image: "/saghara-card.png",
+    link: "#",
+  },
+  {
+    title: "Saghara Labs",
+    image: "/saghara-card.png",
+    link: "#",
+  },
+  {
+    title: "Saghara Labs",
+    image: "/saghara-card.png",
+    link: "#",
+  },
+  {
+    title: "Saghara Labs",
+    image: "/saghara-card.png",
+    link: "#",
+  },
+];
+
+export default function Categories() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-12 py-12">
+      <div className="flex w-full max-w-screen-xl justify-between">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-2xl font-bold text-slate-800">
+            Our website categories
+          </h1>
+          <p className="text-lg text-slate-500">
+            Browse handcrafted templates by category
+          </p>
+        </div>
+        <a href="#" className="flex items-end font-semibold text-slate-800">
+          Explore all categories
+        </a>
+      </div>
+      <div className="grid max-w-screen-xl grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {categoriesData.map((category, index) => (
+          <CategoryCard
+            key={index}
+            title={category.title}
+            image={category.image}
+            link={category.link}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
