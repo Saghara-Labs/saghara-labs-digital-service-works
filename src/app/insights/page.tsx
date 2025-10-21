2bd86c1a6ea27d59498e42ac8e97efc0037b485a
Luqman Herifa <luqmanherifa@gmail.com>
2025-10-21 10:52:33 +0700

    Add About, Services, and Insights Page

---
+++ src/app/insights/page.tsx
@@ -0,0 +1,9 @@
+export default function InsightsPage() {
+  return (
+    <section className="bg-white">
+      <div className="mx-auto max-w-screen-xl py-8 text-center lg:py-16">
+        <h1>Insights</h1>
+      </div>
+    </section>
+  );
+}
