c8303e60e7c350877dd01c25f939e201b5ff445d
Luqman Herifa <luqmanherifa@gmail.com>
2025-10-29 12:22:23 +0700

    Update minor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,10 +30,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen flex-col antialiased`}
       >
         <Navbar />
-        <main>{children}</main>
+        <main className="flex-grow">{children}</main>
         <Footer />
       </body>
     </html>
