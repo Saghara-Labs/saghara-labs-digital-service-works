02e0eb62cbb650b8e1afa45f1be57b47f512c4bb
Luqman Herifa <luqmanherifa@gmail.com>
2025-10-31 09:13:27 +0700

    Add Discover sections

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,15 +1,15 @@
+import Discover from "@/components/sections/Discover";
 import Categories from "@/components/sections/Categories";
 import Services from "@/components/sections/Services";
 import Templates from "@/components/sections/Templates";
 
 export default function ServicesPage() {
   return (
-    <section className="bg-white">
-      <div className="py-8 lg:py-16">
-        <Categories />
-        <Templates />
-        <Services />
-      </div>
-    </section>
+    <>
+      <Discover />
+      <Categories />
+      <Templates />
+      <Services />
+    </>
   );
 }
