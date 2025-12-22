fb71f62d52fb43211a3d911e021d41d06de519d5
Luqman Herifa <luqmanherifa@gmail.com>
2025-12-22 10:15:21 +0700

    Update minor

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,7 +6,7 @@ export default function Button({ label }: ButtonProps) {
   return (
     <button
       type="button"
-      className="cursor-pointer rounded-full bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900"
+      className="cursor-pointer rounded-full bg-gray-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-800"
     >
       {label}
     </button>
